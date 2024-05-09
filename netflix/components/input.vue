4db35ee46da15573eac587e4a0cdb2b624a3ef46
Kentaro Kaneko <38806408+chrptos@users.noreply.github.com>
2024-05-09 12:13:15 +0000

    add: UI component, Resister and Sign in.

---
+++ netflix/components/input.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+// Props interface
+interface Props {
+  id: string;
+  value: string;
+  label: string;
+  type?: string;
+}
+
+// Props with default value for 'type'
+const props = withDefaults(defineProps<Props>(), {
+  type: 'text'
+});
+
+</script>
+
+<template>
+  <div class="relative">
+    <input
+      :id="props.id"
+      :type="props.type"
+      v-model="props.value"
+      class="
+        block
+        rounded-md
+        px-6
+        pt-6
+        pb-1
+        text-md
+        w-full
+        bg-neutral-700
+        focus:outline-none
+        focus:ring-0
+        peer
+      "
+      placeholder=""
+    />
+    <label
+      :for="props.id"
+      class="
+        absolute
+        text-md
+        text-zinc-400
+        duration-150
+        transform
+        -translate-y-3
+        scale-75
+        top-4
+        z-10
+        origin-[0]
+        left-6
+        peer-placeholder-shown:scale-100
+        peer-placeholder-shown:translate-y-0
+        peer-focus:scale-75
+        peer-focus:-translate-y-3
+      "
+    >
+      {{ props.label }}
+    </label>
+  </div>
+</template>
--- netflix/pages/auth.vue
+++ netflix/pages/auth.vue
@@ -1,4 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+const name = ref<string>('');
+const email = ref<string>('');
+const password = ref<string>('');
+const variant = ref<string>('login');
+
+const toggleVariant = () => {
+  variant.value = variant.value === 'login' ? 'register' : 'login';
+};
+
+</script>
 
 <template>
     <div class="relative h-screen w-screen bg-[url('@/assets/images/hero.jpeg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -6,6 +17,27 @@
             <nav class="px-12 py-5">
                 <img src="@/assets/images/logo.png" alt="logo" class="h-12" />
             </nav>
+            <div class="flex justify-center">
+                <div class="bg-black/70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                    <h2 class="text-white text-4xl mb-8 font-semibold">
+                        {{ variant === 'login' ? 'Sign in' : 'Register' }}
+                    </h2>
+                    <div class="flex flex-col gap-4">
+                        <Input v-if="variant === 'register'" label="name" id="name" type="name" :value="name"/>
+                        <Input label="email" id="email" type="email" :value="email"/>
+                        <Input label="password" id="password" type="password" :value="password"/>
+                    </div>
+                    <button class="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        {{ variant === 'login' ? 'Login' : 'Sign up' }}
+                    </button>
+                    <p class="text-neutral-500 mt-12">
+                        {{ variant === 'login' ? 'First time using Netflix?' : 'Already have an account?' }}
+                        <span @click="toggleVariant" class="text-white ml-1 hover:underline cursor-pointer">
+                            {{ variant === 'login' ? 'Create an account' : 'Login' }}
+                        </span>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
