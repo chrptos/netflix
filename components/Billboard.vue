697566a507bc2e35ea84b8812798f556c9989a58
Kentaro Kaneko <38806408+chrptos@users.noreply.github.com>
2024-05-22 15:13:51 +0000

    add: Billboard autoplay video.

---
+++ components/Billboard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+interface Props {
+  visible?: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  visible: false,
+});
+
+const { data, error, pending } = await useFetch('/api/random/');
+
+</script>
+
+<template>
+    <div class="relative h-[56.25vw]">
+      <video autoplay muted loop class="w-full h-[56.25vw] object-cover brightness-[60%]">
+        <source :src="data?.videoUrl" type="video/mp4" />
+      </video>
+    </div>
+</template>
