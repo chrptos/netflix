978b5439f1fff19561538cd08005ea9c9e8f8d75
Kentaro Kaneko <38806408+chrptos@users.noreply.github.com>
2024-05-30 11:36:58 +0000

    add: Movie api.

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
 // definePageMeta({ middleware: 'auth' })
+
+interface Movie {
+  id: string;
+  title: string;
+  description: string;
+  videoUrl: string;
+  thumbnailUrl: string;
+  genre: string;
+  duration: string;
+}
+
+const { data, error, pending } = await useFetch<Movie[]>('/api/movies/');
+
+const movies = ref<Movie[]>(data.value || []);
+
 </script>
 
 <template>
     <div>
         <Navbar />
         <Billboard />
+        <div class="pb-40">
+            <MovieList title="Trending Now" :data="data" />
+        </div>
     </div>
 </template>
